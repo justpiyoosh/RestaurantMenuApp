c4be53fd01a666f1f51ba2c7aa835db0bf9f0d42
justpiyoosh <--global>
2023-08-06 21:13:18 +0530

    Initial Commit

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,15 @@
+class DishesController < ApplicationController
+  
+    def index
+      @dishes = Dish.all
+    end
+  
+    def upload_csv
+      if params[:csv_file].present?
+        uploaded_file = params[:csv_file]
+        CsvWorker.perform_async(uploaded_file.tempfile.path)
+        flash[:notice] = 'CSV file is being processed in the background.'
+      else
+        flash[:alert] = 'Please select a CSV file.'
+      end
+    end
---
+++ db/migrate/20230806152411_create_dishes.rb
@@ -0,0 +1,14 @@
+class CreateDishes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dishes do |t|
+      t.string :dish_name
+      t.text :dish_description
+      t.string :dish_type
+      t.string :allergens
+      t.string :category
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
