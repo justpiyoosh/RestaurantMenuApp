72f40f3abdd70478963e4bcac377df47a4b8e126
justpiyoosh <justpiyoosh@gmail.com>
2023-08-07 23:49:08 +0530

    Add upload_csv to index page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,16 +1,19 @@
 class DishesController < ApplicationController
   
     def index
-      @dishes = Dish.all
+        @dishes = Dish.all
     end
   
     def upload_csv
-      if params[:csv_file].present?
-        uploaded_file = params[:csv_file]
-        CsvWorker.perform_async(uploaded_file.tempfile.path)
-        flash[:notice] = 'CSV file is being processed in the background.'
-      else
-        flash[:alert] = 'Please select a CSV file.'
-      end
+        if params[:csv_file].present?
+          uploaded_file = params[:csv_file]
+        #   CsvWorker.perform_async(uploaded_file.tempfile.path)
+          flash[:notice] = 'CSV file is being processed in the background.'
+        else
+          flash[:alert] = 'Please select a CSV file.'
+        end
+
+        redirect_to dishes_path
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
   root "dishes#index"
+  resources :dishes, only: [:index, :new, :create]
+
+  post 'dishes/upload_csv', to: 'dishes#upload_csv'
 
 end
